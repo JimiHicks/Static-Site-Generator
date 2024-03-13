09910e004d46a47eb22391783f5a457c0a536b60
JimiHicks <jimidavish@gmail.com>
2024-03-13 14:41:42 -0700

    added a test to check bold

---
+++ src/test_inline_markdown.py
@@ -0,0 +1,28 @@
+import unittest 
+from inline_markdown import (
+    split_nodes_delimiter,
+)
+
+from textnode import (
+    TextNode,
+    text_type_text,
+    text_type_bold,
+    text_type_code,
+    text_type_italic,
+)
+
+class TestInlineMarkdown(unittest.TestCase):
+    def test_delim_bold(self):
+        node = TextNode("This is text with a **bolded** word", text_type_text)
+        new_nodes = split_nodes_delimiter([node], "**", text_type_bold)
+        self.assertListEqual(
+            [
+                TextNode("This is text with a ", text_type_text),
+                TextNode("bolded", text_type_bold),
+                TextNode(" word", text_type_text),
+            ],
+            new_nodes,
+        )
+
+if __name__ == "__main__":
+    unittest.main()
