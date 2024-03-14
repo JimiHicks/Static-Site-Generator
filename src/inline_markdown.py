e55427a55c2de13cb115d74a0dea884959b93a73
JimiHicks <jimidavish@gmail.com>
2024-03-13 21:47:07 -0700

    changed text_to_textnode function to work properly, added test

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -10,6 +10,15 @@ from textnode import (
     text_type_link,
 )
 
+def text_to_textnodes(text):
+    nodes = [TextNode(text, text_type_text)]
+    nodes = split_nodes_delimiter(nodes, "**", text_type_bold)
+    nodes = split_nodes_delimiter(nodes, "*", text_type_italic)
+    nodes = split_nodes_delimiter(nodes, "`", text_type_code)
+    nodes = split_image_nodes(nodes)
+    nodes = split_link_nodes(nodes)
+    return nodes
+
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
     for old_node in old_nodes:
@@ -89,8 +98,3 @@ def split_link_nodes(old_nodes):
             new_nodes.append(TextNode(original_text, text_type_text))
     return new_nodes
 
-def text_to_textnodes(text):
-    delim = split_nodes_delimiter(text)
-    image = split_image_nodes(delim)
-    result = split_link_nodes(image)
-    return result
