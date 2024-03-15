8dfb8749331dbfcb9e2d2ddc94e849415c5eb4ad
JimiHicks <jimidavish@gmail.com>
2024-03-15 12:40:49 -0700

    added block type variables

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,11 @@
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_unordered_list = "undordered_list"
+block_type_ordered_list = "ordered_list"
+
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     filtered_blocks = []
