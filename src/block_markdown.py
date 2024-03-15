387df63306596a8156f8e8d525bd877cfe17c653
JimiHicks <jimidavish@gmail.com>
2024-03-15 14:39:17 -0700

    added function to check block type

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,5 @@
+import re
+
 block_type_paragraph = "paragraph"
 block_type_heading = "heading"
 block_type_code = "code"
@@ -16,4 +18,39 @@ def markdown_to_blocks(markdown):
         filtered_blocks.append(block)
     return filtered_blocks
 
-        
+def block_to_block_type(markdown):
+    lines = markdown.split("\n")
+    if (
+        markdown.startswith("# ")
+        or markdown.startswith("## ")
+        or markdown.startswith("### ")
+        or markdown.startswith("#### ")
+        or markdown.startswith("##### ")
+        or markdown.startswith("###### ")
+    ):
+        return block_type_heading
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].endswith("```"):
+        return block_type_code
+    if markdown.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return block_type_paragraph
+        return block_type_quote
+    if markdown.startswith("*"):
+        for line in lines:
+            if not line.startswith("* "):
+                return block_type_paragraph
+        return block_type_unordered_list
+    if markdown.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return block_type_paragraph
+        return block_type_unordered_list
+    if markdown.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i} "):
+                return block_type_paragraph
+            i += 1 
+        return block_type_ordered_list
+    return block_type_paragraph
